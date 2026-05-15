56d8a742f829e0faefba3b7f9aee1d401587f568
Данил Сытников <d.sytnikov@int-sys.ru>
2026-05-15 13:58:24 +0300

    Удалить папку synapse_v43_fixed\(1\)/

--- synapse_v43_fixed(1)/synapse/__init__.py
+++
@@ -1,6 +0,0 @@
-from .app import SynapseApp
-from .device import Device, command
-from .data_fields import state, telemetry, config, event
-
-__version__ = "1.1.0"
-__all__ = ["SynapseApp", "Device", "command", "state", "telemetry", "config", "event"]
